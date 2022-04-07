e96fb33d30822fa137089492a53b3b57f3219015
MohamedAmineAchbah <m.achbah@aui.ma>
2022-04-07 22:45:44 +0000

    Revert "testingDeployment"
    This reverts commit 026cc5d740c34b71897503ea6a7f85b44bc68c41.

---
+++ pages/demo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PaymentForm from 'components/paymenttest/creditCard'
+import TicketTrip from 'components/ticket/ticketTrip'
+
+const demo = () => {
+  return (
+    <div>
+        <TicketTrip/>
+        <PaymentForm/>
+    </div>
+  )
+}
+
+export default demo
